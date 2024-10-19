2b3a85526ce6857ab4ebec7eaa2e823f6c0e4c13
Rob Currie <robert.currie@gmail.com>
2024-10-19 16:31:20 -0700

    Initial conversion to onnx script...not working

--- sims-to-onnx.py
+++ sims-to-onnx.py
@@ -13,7 +13,7 @@ from scsims import SIMS
 import torch.onnx
 
 if __name__ == "__main__":
-    # Load the dataset
+    # Load a sample dataset - onnx requires an example
     adata = sc.read_h5ad("data/pbmc3k_processed.h5ad")
     # Process the dataset
     sc.pp.scale(adata)
@@ -23,13 +23,5 @@ if __name__ == "__main__":
         weights_path="data/11A_2organoids.ckpt", map_location=torch.device("cpu")
     )
 
-    # Perform inference
-    cell_predictions = sims.predict(adata, num_workers=0)
-
-    # Join cell predictions with adata
-    adata.obs.reset_index(
-        inplace=True
-    )  # This is so that the adata.obs index is the same as the cell predictions index
-    adata.obs = adata.obs.join(cell_predictions)
-    # Save the predictions
-    # adata.obs.to_csv(f"predictions/{adata_name}-{checkpoint_name}.csv", index=False)
+    # Save the model as an ONNX
+    sims.model.to_onnx("data/sims.onnx", torch.tensor(adata.X), export_params=True)
