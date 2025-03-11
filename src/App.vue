a29fa1223d743fac4520184674c9a018b8b85eba
Rob Currie <robert.currie@gmail.com>
2025-03-10 19:21:13 -0700

    Add open modal dialog, empty for now, and get claude to change the font durably (deepseek didn't change it so it worked in all screens...

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@
     <v-app-bar>
       <v-toolbar-title>Cell Dimension</v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-btn @click="openFileDialog = true">
+        <v-icon>mdi-folder-open</v-icon>
+      </v-btn>
       <v-btn @click="toggleTheme">
         <v-icon>mdi-theme-light-dark</v-icon>
       </v-btn>
@@ -10,12 +13,24 @@
     <v-main>
       <div>Cell Stray</div>
     </v-main>
+    <v-dialog v-model="openFileDialog" width="500">
+      <v-card>
+        <v-card-title>Open File</v-card-title>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn @click="openFileDialog = false">Close</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-app>
 </template>
 
 <script setup lang="ts">
 import { useTheme } from 'vuetify'
+import { ref } from 'vue'
+
 const theme = useTheme()
+const openFileDialog = ref(false)
 
 function toggleTheme() {
   theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
@@ -24,4 +39,19 @@ function toggleTheme() {
 
 <style>
 @import '@mdi/font/css/materialdesignicons.min.css';
+
+:root {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+.v-application {
+  font-family: inherit;
+  line-height: 1.6;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.v-toolbar-title {
+  font-weight: 600;
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-import '@fontsource/roboto'
 
 import App from './App.vue'
 
@@ -10,13 +9,8 @@ const vuetify = createVuetify({
   components,
   directives,
   theme: {
-    defaultTheme: 'dark'
+    defaultTheme: 'dark',
   },
-  defaults: {
-    VApp: {
-      style: 'font-family: Roboto, sans-serif;'
-    }
-  }
 })
 
 createApp(App).use(vuetify).mount('#app')
